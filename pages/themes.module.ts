2cd118d39a174a9d92dc39d8ba71328aa07ceabd
Honchar Denys <crackeraki@gmail.com>
2024-06-21 01:54:46 +0300

    new user with crud from wacom

--- pages/themes.module.ts
+++ pages/themes.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CoreModule } from 'src/app/core';
+import { CoreModule } from 'src/app/core/core.module';
 import { ThemesComponent } from './themes.component';
 import { Routes, RouterModule } from '@angular/router';
 
